facf0da6b4cacd526729a2af6a8dbbd40cf1c716
Blane Cordes <blane.cordes@gmail.com>
2014-05-08 18:30:52 -0400

    add namespace

--- lib/mandrill_mailer/background_email.rb
+++ lib/mandrill_mailer/background_email.rb
@@ -1,10 +1,12 @@
-class BackgroundEmail < MandrillMailer::TemplateMailer
-  include Sidekiq::Worker
+module MandrillMailer
+  class BackgroundEmail < MandrillMailer::TemplateMailer
+    include Sidekiq::Worker
 
-  default from: 'admin@example.com'
+    default from: 'admin@example.com'
 
-  def perform(args)
-    email = mandrill_mail(args)
-    email.deliver
+    def perform(args)
+      email = mandrill_mail(args)
+      email.deliver
+    end
   end
 end
